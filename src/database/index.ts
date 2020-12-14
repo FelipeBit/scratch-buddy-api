01505c4a188469907115cea5b0bbf03ca67ac31f
felipebit <felipebit>
2020-12-14 05:25:50 -0300

    first release

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,21 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+import DynamicTip from '../models/DynamicTip';
+
+createConnection({
+  type: 'mysql',
+  host: 'databasesb.cuouwoldpnrr.us-east-2.rds.amazonaws.com',
+  port: 3306,
+  username: 'admin',
+  password: '99812827932932',
+  database: 'databasesb',
+  synchronize: true,
+  logging: false,
+  entities: [DynamicTip],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+    entitiesDir: './src/models/',
+  },
+  subscribers: ['./src/subscriber/**/*.ts'],
+});
